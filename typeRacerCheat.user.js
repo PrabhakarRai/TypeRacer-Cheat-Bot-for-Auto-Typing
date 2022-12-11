cad0d8c12bd8382bb1d65414a42695f83536384f
Prabhakar Rai <student343.bhu@gmail.com>
2022-12-11 22:24:15 +0530

    v2

--- typeRacerCheat.user.js
+++ typeRacerCheat.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TypeRacer Cheat
-// @namespace    https://www.madcaphacker.com/
-// @version      1.0
+// @namespace    https://www.madcaped.com/
+// @version      2.0
 // @description  Auto Typing on play.typeracer.com
 // @author       Prabhakar Rai
 // @match        https://play.typeracer.com/
@@ -17,46 +17,43 @@
 // @grant GM_info
 // ==/UserScript==
 
+function triggerKeyboardEvent(el, keyCode, type){
+    var eventObj = document.createEventObject ?
+        document.createEventObject() : document.createEvent("Events");
+
+    if(eventObj.initEvent){
+        eventObj.initEvent(type, true, true);
+    }
+
+    eventObj.keyCode = keyCode;
+    eventObj.which = keyCode;
+
+    el.dispatchEvent ? el.dispatchEvent(eventObj) : el.fireEvent("onkeydown", eventObj);
+}
+
 var typeClear;
+
 function scanner(){
     var a = document.getElementsByClassName('txtInput');
-    if(a.length != 0){
-        if(a[0].disabled == false){
-            clearInterval(lp);
-            function triggerKeyboardEvent(el, keyCode, type)
-            {
-                var eventObj = document.createEventObject ?
-                    document.createEventObject() : document.createEvent("Events");
-
-                if(eventObj.initEvent){
-                    eventObj.initEvent(type, true, true);
-                }
-
-                eventObj.keyCode = keyCode;
-                eventObj.which = keyCode;
-
-                el.dispatchEvent ? el.dispatchEvent(eventObj) : el.fireEvent("onkeydown", eventObj);
-
+    if(a.length != 0 && a[0].disabled == false){
+        clearInterval(lp);
+        var content = document.querySelector("table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(1) > td > div > div").textContent.split('');
+        var i = 0;
+        function a () {
+            if (i == content.length - 1){
+                clearInterval(lp);
+                return;
             }
-            var content = $('table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(1) > td > div > div')[3].textContent.split('');
-            var i = 0;
-            function a () {
-                if (i == content.length - 1){
-                    clearInterval(lp);
-                    return;
-                }
-                var input = $('table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(2) > td > input')[0];
-                input.focus();
-                input.value += content[i++];
-                triggerKeyboardEvent(input, input.value.charCodeAt(0), "keydown");
-                input.click();
-                triggerKeyboardEvent(input, input.value.charCodeAt(0), "keypress");
-                triggerKeyboardEvent(input, input.value.charCodeAt(0), "keyup");
-                console.log(input.value);
-                setTimeout(a, 200 * Math.random());
-            }
-            a();
+            var input = document.querySelector("table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(2) > td > input");
+            input.focus();
+            input.value += content[i++];
+            triggerKeyboardEvent(input, input.value.charCodeAt(0), "keydown");
+            input.click();
+            triggerKeyboardEvent(input, input.value.charCodeAt(0), "keypress");
+            triggerKeyboardEvent(input, input.value.charCodeAt(0), "keyup");
+            setTimeout(a, 200 * Math.random());
         }
+        a();
     }
 }
 var startOver;
@@ -68,6 +65,8 @@ function nextRace(){
             lp = setInterval(scanner, 2000);
         });
     }
+    startOver = undefined;
 }
+
 var lp = setInterval(scanner, 2000);
 var lp2 = setInterval(nextRace, 1500);
